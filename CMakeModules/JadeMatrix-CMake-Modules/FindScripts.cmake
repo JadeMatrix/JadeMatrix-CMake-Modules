0307d49262b69e8bcdbac3776881da6134c1c62e
Joseph Durel <jadematrix.art@gmail.com>
2021-02-21 23:00:09 -0500

    Added a module for adding a directory containing custom CMake Find scripts to the `CMAKE_MODULE_PATH`

---
+++ CMakeModules/JadeMatrix-CMake-Modules/FindScripts.cmake
@@ -0,0 +1,19 @@
+#[=======================================================================[.rst:
+JadeMatrix-CMake-Modules/FindScripts
+--------------------------------------
+
+This module adds a directory containing custom CMake Find scripts to the
+``CMAKE_MODULE_PATH``.
+#]=======================================================================]
+
+CMAKE_MINIMUM_REQUIRED( VERSION 3.3 #[[
+    Requiring 3.3+:
+        IF( ... IN_LIST ... )
+]] )
+
+IF(
+       NOT "${CMAKE_CURRENT_LIST_DIR}/Find/" IN_LIST CMAKE_MODULE_PATH
+    OR NOT "${CMAKE_CURRENT_LIST_DIR}/Find"  IN_LIST CMAKE_MODULE_PATH
+)
+    LIST( APPEND CMAKE_MODULE_PATH "${CMAKE_CURRENT_LIST_DIR}/Find/" )
+ENDIF()
